39e75b27b75d2b582691f24e190cabb84e5eddde
ibreez3 <230584300+ibreez3@users.noreply.github.com>
2026-02-10 16:57:44 +0800

    🐛 修复 sitemap 插件错误：临时禁用 sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ibreez3.github.io',
   base: '/',
-  integrations: [sitemap()],
   build: {
     format: 'file'
   }
